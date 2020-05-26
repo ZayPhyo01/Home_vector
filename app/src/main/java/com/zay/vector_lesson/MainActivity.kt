d2cdc395c1f7ceda13857b0483a042c848a9e3b2
Zay Phyo <zayphyo0099@gmail.com>
2020-05-26 15:44:22 +0630

    animate cloud

--- app/src/main/java/com/zay/vector_lesson/MainActivity.kt
+++ app/src/main/java/com/zay/vector_lesson/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.zay.vector_lesson
 
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.AnimatedVectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val anim = cloudAnim.drawable as Animatable
+        anim.start()
     }
 }
